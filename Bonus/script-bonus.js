967b75671d8d2f2bbfde9fc600e2f429cc7f4cd5
colombovf <colombovf@gmail.com>
2022-11-09 09:17:28 +0100

    Bonus

---
+++ Bonus/script-bonus.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Inserisci nome:");
+let cognome = prompt ("Inserisci cognome:");
+let colore = prompt ("Inserisci il tuo colore preferito:");
+
+console.log (nome+cognome+colore+21);
+
