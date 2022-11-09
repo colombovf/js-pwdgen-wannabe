1e675f925df8e677bc228a64faf7339a2d737f8f
colombovf <colombovf@gmail.com>
2022-11-09 09:45:49 +0100

    Miglioramenti

--- Bonus/script-bonus.js
+++ Bonus/script-bonus.js
@@ -2,5 +2,5 @@ let nome = prompt ("Inserisci nome:");
 let cognome = prompt ("Inserisci cognome:");
 let colore = prompt ("Inserisci il tuo colore preferito:");
 
-console.log (nome+cognome+colore+21);
+console.log (+nome+cognome+colore+21);
 
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let nome = prompt ("Inserisci nome:");
 let cognome = prompt ("Inserisci cognome:");
 let colore = prompt ("Inserisci il tuo colore preferito:");
+let password = nome+cognome+colore+21;
 
-console.log (nome+cognome+colore+21);
+console.log("Nome: "+nome);
+console.log("Cognome: "+cognome);
+console.log("Colore: "+colore);
+console.log ("La tua password è: "+password);
 
+document.getElementById("nomeid").innerHTML= "Nome: "+nome;
+document.getElementById("cognomeid").innerHTML= "Nome: "+cognome;
+document.getElementById("coloreid").innerHTML= "Nome: "+colore;
+document.getElementById("pwdid").innerHTML="La tua password è: "+password;
